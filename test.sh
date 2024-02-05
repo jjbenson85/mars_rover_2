2b86428e8fba080dd8b170fe3307ad5dbbc8a988
jjbenson85 <jjbenson85@gmail.com>
2024-02-05 15:31:22 +0000

    %1%

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 echo 'Running tests...'
 git add .
-npm test --run && git commit -m %1% && echo 'commiting!' || git reset --hard && echo 'Doing a reset!'
+( npm test --run && git commit -m %1% && echo 'commiting!' ) || ( git reset --hard && echo 'Doing a reset!' )
